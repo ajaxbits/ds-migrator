abfface233e057a57df0f19ccc3d663a4b5db85c
Alex Jackson <jaxsalex@pm.me>
2021-03-16 16:52:04 -0500

    add event-based tasks functionality

--- task_mover.py
+++ task_mover.py
@@ -20,27 +20,49 @@ configuration.api_key["api-secret-key"] = user_config["original_api_secret_key"]
 
 # Initialization
 # Set Any Required Values
-api_instance = deepsecurity.ScheduledTasksApi(deepsecurity.ApiClient(configuration))
+scheduled_api_instance = deepsecurity.ScheduledTasksApi(
+    deepsecurity.ApiClient(configuration)
+)
+event_based_api_instance = deepsecurity.EventBasedTasksApi(
+    deepsecurity.ApiClient(configuration)
+)
 api_version = user_config["original_api_version"]
 
 try:
-    task_list = api_instance.list_scheduled_tasks(api_version).scheduled_tasks
+    scheduled_task_list = scheduled_api_instance.list_scheduled_tasks(
+        api_version
+    ).scheduled_tasks
+    event_based_task_list = event_based_api_instance.list_event_based_tasks(
+        api_version
+    ).event_based_tasks
 except ApiException as e:
-    print(
-        "an exception occurred when calling scheduledtasksapi.list_scheduled_tasks: %s\n"
-        % e
-    )
+    print(e)
 
 # change context
 configuration.host = f"{user_config['new_hostname']}:{user_config['new_port']}/api"
 configuration.api_key["api-secret-key"] = user_config["new_api_secret_key"]
 api_version = user_config["new_api_version"]
 
-for old_task in task_list:
+# migrate scheduled tasks
+for old_task in scheduled_task_list:
     try:
-        api_response = api_instance.create_scheduled_task(old_task, api_version)
+        api_response = scheduled_api_instance.create_scheduled_task(
+            old_task, api_version
+        )
     except ApiException as e:
         print(
-            "an exception occurred when calling scheduledtasksapi.list_scheduled_tasks: %s\n"
+            "an exception occurred when calling scheduledtasksapi.create_scheduled_task: %s\n"
+            % e
+        )
+
+# migrate event-based tasks
+for old_task in event_based_task_list:
+    try:
+        api_response = event_based_api_instance.create_event_based_task(
+            old_task, api_version
+        )
+    except ApiException as e:
+        print(
+            "an exception occurred when calling eventbasedtasksapi.create_event_based_task: %s\n"
             % e
         )
