41dc267a489d147bee343ed1682905bace144639
Alex Jackson <jaxsalex@pm.me>
2021-03-17 17:18:05 -0500

    fix dev script

--- dev-setup.sh
+++ dev-setup.sh
@@ -2,5 +2,6 @@
 
 sh <(curl -L https://github.com/numtide/nix-flakes-installer/releases/download/nix-3.0pre20200804_ed52cf6/install)
 . $(eval echo ~$USER)/.nix-profile/etc/profile.d/nix.sh
+mkdir -p ~/.config/nix
 
 echo "nix-command flakes" >> ~/.config/nix/nix.conf
