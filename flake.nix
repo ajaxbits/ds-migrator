03951fe804d91665c437e9f9a9025a4c4f775678
Alex Jackson <45179933+ajaxbits@users.noreply.github.com>
2021-07-19 10:40:41 -0500

    upgrade to python 3.8 and shuffle dependencies

--- flake.nix
+++ flake.nix
@@ -2,26 +2,34 @@
   description = "my project description";
 
   inputs = {
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    stable.url = "github:NixOS/nixpkgs/nixos-20.09";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = inputs.unstable.legacyPackages.${system};
-      env = pkgs.poetry2nix.mkPoetryEnv {
-        projectDir = ./.;
-        python = pkgs.python37;
-      };
-    in {
-      devShell."${system}" = pkgs.mkShell {
-        buildInputs = with pkgs; [ env pkgs.poetry pre-commit ];
-        shellHook = ''
-          mkdir -p .vim
-          echo '{"python.pythonPath": "${env}/bin/python", "python.formatting.provider": "black", "python.formatting.blackPath": "${env}/bin/black", "coc.preferences.formatOnSaveFiletypes":["python"]}' > .vim/coc-settings.json
-          pre-commit install -f --hook-type pre-commit --allow-missing-config
-        '';
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        dsmigrator-env = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          python = pkgs.python38;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            poetry
+            dsmigrator-env
+            python38Packages.pylint
+            python38Packages.black
+            python38Packages.nose2
+            python38Packages.rope
+            pre-commit
+          ];
+          shellHook = ''
+            mkdir -p .vim
+            echo '{"python.pythonPath": "${dsmigrator-env}/bin/python", "python.formatting.provider": "black", "coc.preferences.formatOnSaveFiletypes":["python"]}' > .vim/coc-settings.json
+            pre-commit install -f --hook-type pre-commit --allow-missing-config
+          '';
+        };
+      });
 }
